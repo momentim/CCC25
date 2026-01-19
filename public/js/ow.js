580863d0e60e9d328b92ab15aa42f629dd1d0291
momentim <46795703+momentim@users.noreply.github.com>
2026-01-19 12:52:35 +0000

    Stop tracking public and resources folders

--- public/js/ow.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    var s = document.createElement('script');
-    s.src = 'https://widget.operationworld.org/js/widget.js';
-    s.async = true;
-    document.body.appendChild(s);
-}());
